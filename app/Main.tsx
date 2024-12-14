f69b1fe7ac36e3d7e9971d4ef39c8d23c38f9c70
murph <murphyxiaoxi@outlook.com>
2024-12-14 20:37:49 +0800

    mark

--- app/Main.tsx
+++ app/Main.tsx
@@ -12,14 +12,14 @@ export default function Home({ posts }) {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            {/* Latest */}
-          </h1>
+          {/* <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+            Latest
+          </h1> */}
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
 
-          <div className="mt-0 flex flex-col items-center">
+          <div className="mt-0 flex flex-col items-center md:items-start">
             {/* <div className="mb-8 text-sm text-gray-500 dark:text-gray-400">
                 Tailwind Nextjs Theme
             </div> */}
@@ -102,7 +102,7 @@ export default function Home({ posts }) {
       {posts.length > MAX_DISPLAY && (
         <div className="flex justify-end text-base font-medium leading-6">
           <Link
-            href="/blog"
+            href="/blog/articles"
             className="text-primary-500 hover:text-primary-600 dark:hover:text-primary-400"
             aria-label="All posts"
           >
